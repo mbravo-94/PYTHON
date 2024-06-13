026449bb1151691ddc81462fd891b65f193e94d7
Marina Bravo <marinabravo94@gmail.com>
2024-06-13 23:55:03 +0200

    arriba, abajo, derecha, izquierda

--- Juego/main.py
+++ Juego/main.py
@@ -1,5 +1,9 @@
 # %%
 import pygame
+import os
+
+# Asegurando que el directorio de trabajo es el correcto
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # Iniciando Pygame
 pygame.init()
@@ -10,17 +14,54 @@ pygame.display.set_caption('Space Invaders')
 icon = pygame.image.load('space-invaders.png')
 pygame.display.set_icon(icon)
 
-# Game Loop
-running = True
-while running:
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT:
-            running = False
+# Jugador
+playerImg = pygame.image.load('spaceship.png')
+playerX = 370
+playerY = 480
+playerX_change = 0  # Inicializar la variable de cambio de posición en X
+playerY_change = 0  # Inicializar la variable de cambio de posición en Y
 
-    screen.fill((0, 0, 0))  # Rellenar la pantalla con color negro
-    pygame.display.update()   # Actualizar la pantalla
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
-# Salir de Pygame
-pygame.quit()
+def game_loop():
+    global playerX, playerY, playerX_change, playerY_change
+    running = True
+    while running:
+        # RGB (Red, Green, Blue)
+        screen.fill((0, 0, 0))  # Rellenar la pantalla con color negro
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            
+            # Pulsar el teclado
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    playerX_change = 0.2
+                if event.key == pygame.K_LEFT:
+                    playerX_change = -0.2
+                if event.key == pygame.K_UP:
+                    playerY_change = -0.2
+                if event.key == pygame.K_DOWN:
+                    playerY_change = 0.2
+
+            # Soltar el teclado
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    playerY_change = 0
+
+        playerX += playerX_change
+        playerY += playerY_change
+        player(playerX, playerY)
+        pygame.display.update()  # Actualizar la pantalla
+
+    # Salir de Pygame
+    pygame.quit()
+
+if __name__ == "__main__":
+    game_loop()
 
 # %%
